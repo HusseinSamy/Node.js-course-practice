a2ef2d8185eb0ab5064b1a0fc1fda572294eda8c
Hussein Samy <hussein.samy02@gmail.com>
2023-04-04 06:19:26 +0200

    Assigment 1 in S5 - Creates simple express.js app

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 /** @format */
+const { log } = require("console");
+const express = require("express");
 
-const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+app.use("/users", (req, res, next) => {
+ console.log("Hello World From the second server using Express");
+ res.send("what are you doing?");
+});
+app.use(
+ "/",
+ (req, res, next) => {
+  console.log("Hello World From the first server using Express");
+  next();
+ },
+ (req, res, next) => {
+  console.log("This is the second middleware in the / route");
+  res.send("I have done what you want");
+ }
+);
+app.listen(3000);
