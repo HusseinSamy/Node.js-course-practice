a3ff583381863cd421f297721b67d574f400bff0
Hussein Samy <hussein.samy02@gmail.com>
2023-04-05 08:52:05 +0200

    Handles non-registered routes

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(adminRoute);
 app.use(shopRoute);
+app.use((req, res, next) => {
+ res.status(404).send("<h1>404 - Page is not found.<h1/>");
+});
 
 // Server Loop
 app.listen(3000);
