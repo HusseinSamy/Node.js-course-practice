42f5187c4488ca5f6189cbf5b314eaa3c4a613d7
Hussein Samy <hussein.samy02@gmail.com>
2023-04-11 23:31:40 +0200

    Section 6 start

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,20 @@
-/** @format */
+const path = require('path');
 
-const express = require("express");
-const path = require("path")
+const express = require('express');
+
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-router.get("/add-user", (req, res, next) => {
-    res.sendFile(path.join(__dirname, "../", "views", "add-user.html"));
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post("/users", (req, res, next) => {
- console.log(req.body);
- res.send(req.body);
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 module.exports = router;
